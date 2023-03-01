d9d238d5308b62fd581f2f8ae02f9bc008a050e3
team5131 <73025791+TeamSinkong@users.noreply.github.com>
2023-03-01 11:53:35 +0700

    new

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-const target={clicked:0,currentFollowers:300,btn:document.querySelector("a.btn"),fw:document.querySelector("span.followers")};const follow=()=>{target.clicked+=1;target.btn.innerHTML='Following <i class="fas fa-user-times"></i>';if(target.clicked%2===0){target.currentFollowers-=1;target.btn.innerHTML='Follow <i class="fas fa-user-plus"></i>';}
-else{target.currentFollowers+=1;}
-target.fw.textContent=target.currentFollowers;target.btn.classList.toggle("following");}
+function loadFile(filePath) {
+    var result = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", filePath, false);
+    xmlhttp.send();
+    if (xmlhttp.status == 200) {
+        result = xmlhttp.response;
+        let dom = new DOMParser()
+            .parseFromString(result, 'text/html');
+        let body = dom.body;
+        console.log(body)
+        let files = dom.getElementById("files")
+        let i = 0
+        for (const child of files.children) {
+            if (i > 0) {
+                let bodyhtml = document.getElementById("videoA")
+                let video = document.createElement("video")
+                video.src = "/video/" + child.children.item(0).title
+                video.width = 320
+                video.height = 240
+                video.controls = true
+                bodyhtml.appendChild(video)
+            }
+            i++
+        }
+    }
+    return result;
+}
+
+loadFile('../video/')
